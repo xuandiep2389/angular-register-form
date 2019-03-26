8a8a020ccd1237bec0d92000246d12d4343e7c6d
vu linh <xuandiep2389@gmail.com>
2019-03-26 10:05:07 +0700

    validate first name, last name and email

--- src/app/reg-form/reg-form.component.ts
+++ src/app/reg-form/reg-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reg-form',
@@ -11,16 +11,24 @@ export class RegFormComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   regForm = this.formBuilder.group({
-    firstName:[''],
-    lastName: [''],
-    email: [''],
+    firstName:['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
     dob: [''],
     gender: [''],
     city: ['']
   });
 
   ngOnInit() {
+
   }
+
+  get firstName() { return this.regForm.get('firstName')};
+  get lastName() { return this.regForm.get('lastName')};
+  get email() { return this.regForm.get('email')};
+  get dob() { return this.regForm.get('dob')};
+  get gender() { return this.regForm.get('gender')};
+  get city() { return this.regForm.get('city')};
 
   onSubmit(){
     console.warn(this.regForm.value);
